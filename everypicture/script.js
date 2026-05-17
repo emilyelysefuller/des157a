b7106a30919e87d7dc342e1d3ea9f3b330d469a6
Emily Fuller <emilyelysefuller@gmail.com>
2026-05-16 19:16:19 -0700

    final every picture project

--- everypicture/script.js
+++ everypicture/script.js
@@ -2,9 +2,6 @@
     'use strict';
     console.log('reading JS');
 
-
-
-
     // Making all reels continouous- no jumping- used "09-ImageScroll-left" files as example, and did that for every slider
     window.addEventListener('load', function () {
 
@@ -117,5 +114,123 @@
     document.querySelector('.slider-f').classList.add("animate-f");
 
 });
+
+    // Image Mapping for each object, I named every svg object, object1, 2, etc. and then used this to find which one was witch // 
+
+    document.querySelector('#photos').addEventListener('pointerenter', function(){
+        console.log('over photos');
+    });
+
+    document.querySelector('#keys').addEventListener('pointerenter', function(){
+        console.log('over keys');
+    });
+
+    document.querySelector('#plant').addEventListener('pointerenter', function(){
+        console.log('over plants');
+    });
+
+    document.querySelector('#shells').addEventListener('pointerenter', function(){
+        console.log('over shells');
+    });
+
+    document.querySelector('#cat').addEventListener('pointerenter', function(){
+        console.log('over cat');
+    });
+
+    document.querySelector('#music').addEventListener('pointerenter', function(){
+        console.log('over concert bands');
+    });
+
+    // adding and removing outlines on hover // 
+    const photos = document.querySelector('#photos');
+    const keys = document.querySelector('#keys');
+    const plant = document.querySelector('#plant');
+    const shells = document.querySelector('#shells');
+    const music = document.querySelector('#music');
+    const cat = document.querySelector('#cat');
+
+    photos.addEventListener('pointerenter', function(){
+        photos.classList.add('show-object');
+    });
+
+    photos.addEventListener('pointerleave', function(){
+        photos.classList.remove('show-object');
+    });
+
+    keys.addEventListener('pointerenter', function(){
+        keys.classList.add('show-object');
+    });
+
+    keys.addEventListener('pointerleave', function(){
+        keys.classList.remove('show-object');
+    });
+
+    plant.addEventListener('pointerenter', function(){
+        plant.classList.add('show-object');
+    });
+
+    plant.addEventListener('pointerleave', function(){
+        plant.classList.remove('show-object');
+    });
+
+    shells.addEventListener('pointerenter', function(){
+        shells.classList.add('show-object');
+    });
+
+    shells.addEventListener('pointerleave', function(){
+        shells.classList.remove('show-object');
+    });
+
+    music.addEventListener('pointerenter', function(){
+        music.classList.add('show-object');
+    });
+
+    music.addEventListener('pointerleave', function(){
+        music.classList.remove('show-object');
+    });
+
+    cat.addEventListener('pointerenter', function(){
+        cat.classList.add('show-object');
+    });
+
+    cat.addEventListener('pointerleave', function(){
+        cat.classList.remove('show-object');
+    });
+
+    //adding story/reel change on hover of specific oject// 
     
+    const storyPanels = document.querySelectorAll('.story-panel');
+
+    function showPanel(panelId) {
+        storyPanels.forEach(function(panel) {
+            panel.className = 'story-panel hidden';
+        });
+
+        document.querySelector(panelId).className = 'story-panel visible';
+    }
+
+    photos.addEventListener('pointerenter', function(){
+        showPanel('#polaroids-panel');
+    });
+
+    keys.addEventListener('pointerenter', function(){
+        showPanel('#keys-panel');
+    });
+
+    plant.addEventListener('pointerenter', function(){
+        showPanel('#plants-panel');
+    });
+
+    shells.addEventListener('pointerenter', function(){
+        showPanel('#shells-panel');
+    });
+
+    music.addEventListener('pointerenter', function(){
+        showPanel('#concerts-panel');
+    });
+
+    cat.addEventListener('pointerenter', function(){
+        showPanel('#cats-panel');
+    });
+
 })();
